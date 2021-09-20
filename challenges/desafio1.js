0edbe823b0e5b4d83b588e6473e806b679c8a294
Marco Galindo <marco1408@gmail.com>
2021-09-20 19:16:33 -0300

    desafio 1 finalizado

---
+++ challenges/desafio1.js
@@ -0,0 +1,12 @@
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": {
+        $gte: 7,
+      },
+      rated: { $in: ["PG", "G"] },
+      genres: { $nin: ["Crime", "Horror"] },
+      languages: { $all: ["English", "Spanish"] },
+    },
+  },
+]);
