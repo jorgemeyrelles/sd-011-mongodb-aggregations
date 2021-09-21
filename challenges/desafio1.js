e91f3860e967768c9b714d5b56206654f8d46b92
MatheusAraujoDev <matheusaraujo.dev@gmail.com>
2021-09-21 14:29:17 -0300

    iniciando o projeto MongoDB aggregations

---
+++ challenges/desafio1.js
@@ -0,0 +1,12 @@
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        { "imb.rating": { $gte: 7 } },
+        { genres: { $nin: [ "Crime", "Horror" ] } },
+        { rated: { $in: [ "PG", "G" ] } },
+        { languages: { $in: [ "English", "Spanish" ] } }
+      ]
+    },
+  }
+]);
