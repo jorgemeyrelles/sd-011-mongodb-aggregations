465f111fe1c34f394d9e240bb4523d9fa6ab0116
heyset <c.inaciomatheus@gmail.com>
2021-09-21 19:39:16 -0300

    Adiciona desafio 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,29 @@
+db.air_alliances.aggregate([
+  {
+    $lookup: {
+      from: "air_routes",
+      as: "routes_targeted_airplanes",
+      let: { alliance_airlines: "$airlines" },
+      pipeline: [
+        { $match: {
+            $expr: { $and: [
+              { $in: ["$airplane", ["747", "380"]] },
+              { $in: ["$airline.name", "$$alliance_airlines"] },
+            ] },
+        } },
+      ],
+    },
+  },
+  {
+    $project: {
+      _id: "$name",
+      totalRotas: { $size: "$routes_targeted_airplanes" },
+    },
+  },
+  {
+    $sort: { totalRotas: -1 },
+  },
+  {
+    $limit: 1,
+  },
+]);
