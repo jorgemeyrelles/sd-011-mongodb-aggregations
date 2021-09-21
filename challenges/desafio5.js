17d8f8c63391299e8b756f6d9467c25062d62c23
Marco Galindo <marco1408@gmail.com>
2021-09-20 22:17:00 -0300

    desafio 5, 6 e 7 finalizados

---
+++ challenges/desafio5.js
@@ -0,0 +1,55 @@
+db.movies.aggregate([
+  {
+    $match: {
+      countries: "USA",
+      "tomatoes.viewer.rating": { $gte: 3 },
+      cast: {
+        $in: [
+          "Sandra Bullock",
+          "Tom Hanks",
+          "Julia Roberts",
+          "Kevin Spacey",
+          "George Clooney",
+        ],
+      },
+    },
+  },
+  {
+    $addFields: {
+      actors: {
+        $setIntersection: [
+          [
+            "Sandra Bullock",
+            "Tom Hanks",
+            "Julia Roberts",
+            "Kevin Spacey",
+            "George Clooney",
+          ],
+          "$cast",
+        ],
+      },
+    },
+  },
+  {
+    $addFields: {
+      num_favs: {
+        $size: "$actors",
+      },
+    },
+  },
+  {
+    $sort: {
+      num_favs: -1,
+      "tomatoes.viewer.rating": -1,
+      title: -1,
+    },
+  },
+  { $skip: 24 },
+  { $limit: 1 },
+  {
+    $project: {
+      _id: 0,
+      title: 1,
+    },
+  },
+]);
---
+++ challenges/desafio7.js
@@ -0,0 +1,30 @@
+db.movies.aggregate([
+  {
+    $match: {
+      languages: {
+        $in: ["English"],
+      },
+    },
+  },
+  { $unwind: "$cast" },
+  {
+    $group: {
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      mediaImdb: { $avg: "$imdb.rating" },
+    },
+  },
+  {
+    $sort: {
+      numeroFilmes: -1,
+      _id: -1,
+    },
+  },
+  {
+    $project: {
+      _id: 1,
+      numeroFilmes: 1,
+      mediaImdb: { $round: ["$mediaImdb", 1] },
+    },
+  },
+]);
