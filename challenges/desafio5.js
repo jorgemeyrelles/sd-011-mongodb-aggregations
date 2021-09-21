a9d970fe9c3b5d231eeb616f734f34df2ad1c9d0
anaclarabck <ackyotoku@gmail.com>
2021-09-21 16:07:29 -0300

    [UPDATE] fixed linter

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -2,25 +2,25 @@ db.movies.aggregate([
   { $match: {
     "tomatoes.viewer.rating": { $gte: 3 },
     countries: { $in: ["USA"] },
-    cast: { $exists: true},
-} },
-{ $addFields: {
-  favs_intersection: { $let: {
-    vars: {
-      people_fav: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"] },
-    in: { $setIntersection: ["$$people_fav", "$cast"] }
-} } } },
-{ $addFields: { num_favs: { $size: "$favs_intersection" },
-} },
-{ $sort: {
-  num_favs: -1,
-  "tomatoes.viewer.rating": -1,
-  title: -1,
-} },
-{ $skip: 24 },
-{ $limit: 1 },
-{ $project: {
-  _id: 0,
-  title: 1,
-} },
+    cast: { $exists: true },
+  } },
+  { $addFields: {
+    favs_intersection: { $let: {
+      vars: {
+        people_fav: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"] },
+      in: { $setIntersection: ["$$people_fav", "$cast"] }
+    } } } },
+  { $addFields: { num_favs: { $size: "$favs_intersection" },
+  } },
+  { $sort: {
+    num_favs: -1,
+    "tomatoes.viewer.rating": -1,
+    title: -1,
+  } },
+  { $skip: 24 },
+  { $limit: 1 },
+  { $project: {
+    _id: 0,
+    title: 1,
+  } },
 ]);
