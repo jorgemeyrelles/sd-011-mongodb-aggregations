f18de843c970439e9effab9f389fa5c5c6a97bd6
Marco Galindo <marco1408@gmail.com>
2021-09-21 15:36:37 -0300

    desafio 10 finalizado

---
+++ challenges/desafio10.js
@@ -0,0 +1,26 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      difTime: {
+        $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 3600000],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: {
+        $avg: "$difTime",
+      },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: {
+        $round: ["$duracaoMedia", 2],
+      },
+    },
+  },
+]);
