88d97f23765718d018e178d107adc0641d2a5007
Filizzola <juliofilizzola@hotmail.com>
2021-09-21 08:15:26 -0300

    fix desafio 8 | indentação

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -3,18 +3,18 @@
 db.air_alliances.aggregate([
   {
     $lookup: {
-      from: 'air_routes',
-      localField: 'airlines',
-      foreignField: 'airline.name',
-      as: 'airplaneMatch'
-    }
-  }, 
-  { $unwind: "$airplaneMatch"},
+      from: "air_routes",
+      localField: "airlines",
+      foreignField: "airline.name",
+      as: "airplaneMatch",
+    },
+  },
+  { $unwind: "$airplaneMatch" },
   { $match: { "airplaneMatch.airplane": { $in: ["747", "380"] } } },
   { $group: {
     _id: "$name",
-    totalRotas: { $sum: 1 }
+    totalRotas: { $sum: 1 },
   } },
   { $sort: { totalRotas: -1 } },
-  { $limit: 1 }
+  { $limit: 1 },
 ]);
