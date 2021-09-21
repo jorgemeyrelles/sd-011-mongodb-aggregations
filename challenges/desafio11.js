43e52c44912af4444e2f587fef0bf9df003069b9
Thiagofox <thiagofox00@gmail.com>
2021-09-21 14:39:16 -0300

    criaçao dos arquivos

---
+++ challenges/desafio11.js
@@ -0,0 +1,10 @@
+/* Desafio 11
+
+Determine qual o dia da semana com maior número de viagens iniciadas.
+
+Dica: Utilize o operador $dayOfWeek para extrair o dia da semana como um número de uma data.
+
+O resultado da sua query deve ter exatamente o seguinte formato (incluindo a ordem dos campos):
+
+{ "diaDaSemana" : <dia_da_semana>, "total" : <total_de_viagens> }
+*/
