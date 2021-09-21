890d1897db02bc2db81e99320a43069ff7e3afc7
PH Assis <pieit@live.com>
2021-09-21 17:14:46 -0300

    Added challenge 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,14 @@
+// use("aggregations");
+db.trips.aggregate([
+  { $match: { birthYear: { $ne: "" } } },
+  { $project: { _id: 0, birthYear: { $toInt: "$birthYear" } } },
+  {
+    $group: {
+      _id: null,
+      maiorAnoNascimento: { $max: "$birthYear" },
+      menorAnoNascimento: { $min: "$birthYear" },
+    },
+  },
+  { $project: { _id: 0, maiorAnoNascimento: 1, menorAnoNascimento: 1 } },
+]);
+// .itcount();
