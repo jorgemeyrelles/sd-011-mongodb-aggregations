5aeb05460be84cc19aab8c96e97dda3831b78f8e
Gui Pereira <guilhermepdo1508@gmail.com>
2021-09-21 17:03:22 -0300

    Desafio 4 feito

---
+++ challenges/desafio4.js
@@ -0,0 +1,10 @@
+db.movies.aggregate([
+  { $project: {
+    _id: 0,
+    title: 1,
+    title_split: { $split: ["$title", " "] },
+  } },
+  { $match: { title_split: { $size: 1 } } },
+  { $sort: { title: 1 } },
+  { $project: { title: 0 } },
+]);
