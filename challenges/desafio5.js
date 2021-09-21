d24e5935e6a7be8f33ed85efaa49cbcbb3704ddc
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-09-21 18:52:35 -0300

    correçao desafio 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,25 +1,42 @@
+const favActors = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"];
+
 db.movies.aggregate([
   {
     $match: {
       countries: "USA",
       "tomatoes.viewer.rating": { $gte: 3 },
-      cast: { $all: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"] },
+      cast: { $exists: true },
+    },
+  },
+  {
+    $addFields: {
+      num_favs_array: {
+        $setIntersection: ["$cast", favActors],
+      },
+    },
+  },
+  {
+    $match: {
+      num_favs_array: {
+        $setIntersection: ["$cast", favActors],
+      },
+    },
+  },
+  {
+    $addFields: {
+      num_favs: {
+        $size: "$num_favs_array",
+      },
     },
   },
   {
     $project: {
       title: 1,
       _id: 0,
-      num_favs: {
-        $size: { $setIntersection: ["$cast", ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"]] },
-      },
       cast: 1,
     },
   },
   {
     $skip: 25,
-  },
-  {
-    $limit: 1,
   },
 ]);
