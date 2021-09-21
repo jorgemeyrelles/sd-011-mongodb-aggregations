d6cb862ad8145e0d9a246f0363efbbb787b5a281
Hugosomers <hugosomersla@gmail.com>
2021-09-21 14:50:30 -0300

    Desafio 1 fix

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,8 +1,8 @@
 db.movies.aggregate([
   { $match: {
     "imdb.rating": { $gte: 7 },
-    genres: { $nin: ["Crime", "Horror"] },
-    rated: { $in: ["PG", "G"] },
-    languages: { $in: ["English", "Spanish"] },
+    "genres": { $nin: ["Crime", "Horror"] },
+    "rated": { $in: ["PG", "G"] },
+    "languages": { $all: ["English", "Spanish"] },
   } },
 ]);
