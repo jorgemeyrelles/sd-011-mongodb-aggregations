66d3d5f9445b821a09494d3e4a66f23ab8fd0dd6
KaioHPs <kaiohpsalternativo@gmail.com>
2021-09-21 17:52:03 -0300

    Requisitos 1 a 7

---
+++ challenges/desafio5.js
@@ -0,0 +1,6 @@
+db.movies.aggregate([{ $match: { countries: { $all: ["USA"] }, "tomatoes.viewer.rating": { $gte: 3 }, cast: { $exists: true } } },
+  { $addFields: { num_favs: { $let: {
+    vars: { favs: ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"] },
+    in: { $size: { $setIntersection: ["$cast", "$$favs"] } },
+  } } } },
+  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } }, { $project: { _id: 0, title: 1 } }, { $skip: 24 }, { $limit: 1 }]);
