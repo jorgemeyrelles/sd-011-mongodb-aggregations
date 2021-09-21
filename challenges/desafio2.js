e861e5ca5131c36695fa231635045bede8afc778
gabiazevedo <gabiazevedoms@gmail.com>
2021-09-21 14:03:11 -0300

    Finaliza desafio 2, 3

---
+++ challenges/desafio2.js
@@ -0,0 +1,24 @@
+db.movies.aggregate([
+  {
+    $match: { languages: { $all: ["English", "Spanish"] } },
+  },
+  {
+    $match: { rated: { $in: ["PG", "G"] } },
+  },
+  {
+    $match: { genres: { $nin: ["Crime", "Horror"] } },
+  },
+  {
+    $match: { "imdb.rating": { $gte: 7 } },
+  },
+  {
+    $project: {
+      _id: 0,
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
+]);
---
+++ challenges/desafio3.js
@@ -0,0 +1,31 @@
+db.movies.aggregate([
+  {
+    $match: { languages: { $all: ["English", "Spanish"] } },
+  },
+  {
+    $match: { rated: { $in: ["PG", "G"] } },
+  },
+  {
+    $match: { genres: { $nin: ["Crime", "Horror"] } },
+  },
+  {
+    $match: { "imdb.rating": { $gte: 7 } },
+  },
+  {
+    $project: {
+      _id: 0,
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
+  {
+    $sort: {
+      ano: -1,
+      notaIMDB: -1,
+      titulo: 1,
+    },
+  },
+]);
