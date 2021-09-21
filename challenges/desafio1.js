3e1e4bc5ad2ecbe7864da916fe5aaee2b66f2f08
Marcelo <marsleite@gmail.com>
2021-09-21 20:01:16 -0300

    desafio 1 - feito

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,1 +1,11 @@
 // Desafio 1
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
+    },
+  },
+]);
