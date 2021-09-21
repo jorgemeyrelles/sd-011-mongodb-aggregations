a0d9cecb2d02fb8b8d765c88195db5e9c44b174b
Marco Galindo <marco1408@gmail.com>
2021-09-21 17:17:14 -0300

    requisito 10 corrigido

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,16 +1,11 @@
 db.trips.aggregate([
   {
-    $addFields: {
-      difTime: {
-        $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 3600000],
-      },
-    },
-  },
-  {
     $group: {
       _id: "$usertype",
       duracaoMedia: {
-        $avg: "$difTime",
+        $avg: {
+          $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 3600000],
+        },
       },
     },
   },
