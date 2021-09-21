c57b1214a0c6a49a265d276e41091bced0d6b0ae
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-09-21 12:54:27 -0300

    desafio 3 - lint

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,1 +1,16 @@
-db.movies.aggregate([{ $match: { "imdb.rating": { $gte: 7 } } }, { $match: { genres: { $nin: ["Crime", "Horror"] } } }, { $match: { rated: { $in: ["PG", "G"] } } }, { $match: { languages: { $all: ["English", "Spanish"] } } }, { $addFields: { titulo: "$title", avaliado: "$rated", notaIMDB: "$imdb.rating", votosIMDB: "$imdb.votes", ano: "$year" } }, { $project: { titulo: 1, avaliado: 1, notaIMDB: 1, votosIMDB: 1, ano: 1, _id: 0 }}, {$sort: {"ano": -1, "notaIMDB": -1, "título": 1}} ]);
+db.movies.aggregate([{ $match: { "imdb.rating": { $gte: 7 } } },
+  { $match: { genres: { $nin: ["Crime", "Horror"] } } },
+  { $match: { rated: { $in: ["PG", "G"] } } },
+  { $match: { languages: { $all: ["English", "Spanish"] } } },
+  { $addFields: { titulo: "$title",
+    avaliado: "$rated",
+    notaIMDB: "$imdb.rating",
+    votosIMDB: "$imdb.votes",
+    ano: "$year" } },
+  { $project: { titulo: 1,
+    avaliado: 1,
+    notaIMDB: 1,
+    votosIMDB: 1,
+    ano: 1,
+    _id: 0 } },
+  { $sort: { ano: -1, notaIMDB: -1, título: 1 } }]);
