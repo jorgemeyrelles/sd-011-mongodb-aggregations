4191900de10f93b6d028db1c7bc93b09090f141e
Tales Coelho dos Santos <tales-coelho@live.com>
2021-09-21 17:16:08 -0300

    Update desafio7.js

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,11 +1,11 @@
 db.movies.aggregate(
   {
-    $match: { languages: { $in : ["English"] } },
+    $match: { languages: { $in: ["English"] } },
   },
-  { $unwind:  "$cast" },
+  { $unwind: "$cast" },
   {
-    $group: { 
-      _id: "$cast", 
+    $group: {
+      _id: "$cast",
       numeroFilmes: { $sum: 1 },
       mediaIMDB: { $avg: "$imdb.rating" },
     },
