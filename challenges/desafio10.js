b7d9bd4a585ee057d55c7294a56b702abfe167c8
Diego Ribeiro <dihrm2001@gmail.com>
2021-09-21 16:35:10 -0300

    Desafio 10

---
+++ challenges/desafio10.js
@@ -0,0 +1,18 @@
+const hours = 60 * 60 * 1000;
+
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: { $avg: { $subtract: ["$stopTime", "$startTime"] } },
+    },
+  },
+  { $sort: { duracaoMedia: 1 } },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: { $round: [{ $divide: ["$duracaoMedia", hours] }, 2] },
+    },
+  },
+]);
