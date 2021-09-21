4124be74a6749b2763c3fa73dc5c3a0a48fe9aa0
Tales Coelho dos Santos <tales-coelho@live.com>
2021-09-21 17:06:21 -0300

    ajuste lint

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,18 +1,26 @@
 db.movies.aggregate(
-  { $match: { awards: { $regex: /won/i } } },
-  { $match: { awards: { $regex: /oscars/i } } },
-  { $group: {
-    _id: "Vida longa e prospera",
-    maior_rating : { $max: "$imdb.rating" },
-    menor_rating : { $min: "$imdb.rating" },
-    media_rating : { $avg: "$imdb.rating" },
-    desvio_padrao : { $stdDevSamp: "$imdb.rating" },
-  } },
-  { $project: {
-    _id: 0,
-    maior_rating: 1,
-    menor_rating: 1,
-    media_rating: { $round: ["$media_rating", 1] },
-    desvio_padrao: { $round: ["$desvio_padrao", 1] },
-  }}
+  {
+    $match: { awards: { $regex: /won/i } },
+  },
+  {
+    $match: { awards: { $regex: /oscars/i } },
+  },
+  {
+    $group: {
+      _id: "Vida longa e prospera",
+      maior_rating : { $max: "$imdb.rating" },
+      menor_rating : { $min: "$imdb.rating" },
+      media_rating : { $avg: "$imdb.rating" },
+      desvio_padrao : { $stdDevSamp: "$imdb.rating" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      maior_rating: 1,
+      menor_rating: 1,
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
+    },
+  },
 );
