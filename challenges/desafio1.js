ebbd08a72ad57cab16bc0e0e3ed1fcaf33135d35
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-09-21 17:54:40 -0300

    feito 1, 2 e 3

---
+++ challenges/desafio1.js
@@ -0,0 +1,14 @@
+db.movies.aggregate([
+  {
+    $match:
+      {
+        $and:
+        [
+          { genres: { $nin: ["Crime", "Horror"] }  },
+          { "imdb.rating": { $gte: 7 } },
+          { rated: { $in: ["PG", "G"] } },
+          { languages: { $all: ["English", "Spanish"] } },
+        ]
+      },
+  },
+]);
---
+++ challenges/desafio2.js
@@ -0,0 +1,23 @@
+db.movies.aggregate([
+  {
+    $match:
+      {
+        $and:
+        [
+          { genres: { $nin: ["Crime", "Horror"] }  },
+          { "imdb.rating": { $gte: 7 } },
+          { rated: { $in: ["PG", "G"] } },
+          { languages: { $all: ["English", "Spanish"] } },
+        ]
+      },
+  },
+  {
+    $project: {
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$ano"
+    }
+  }
+]);
