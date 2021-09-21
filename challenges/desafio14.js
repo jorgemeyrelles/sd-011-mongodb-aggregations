aac9818630d866ef9f8fa35c90c2f686ca25bc13
rodolforezende <rodolforrcont@gmail.com>
2021-09-21 17:55:23 -0300

    Desafio 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,35 @@
+db.trips.aggregate(
+  {
+    $addFields: {
+      tempoMedio: {
+        $divide: [
+          { $subtract: ["$stopTime", "$startTime"] },
+          60 * 1000,
+        ],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$bikeid",
+      duracaoMedia: {
+        $avg: "$tempoMedio",
+      },
+    },
+  },
+  {
+    $sort: {
+      duracaoMedia: -1,
+    },
+  },
+  {
+    $limit: 5,
+  },
+  {
+    $project: {
+      _id: 0,
+      bikeId: "$_id",
+      duracaoMedia: { $ceil: "$duracaoMedia" },
+    },
+  },
+);
