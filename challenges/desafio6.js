41eb4222994e8730d1059e0c0d76da863094a2d8
Tales Coelho dos Santos <tales-coelho@live.com>
2021-09-21 17:08:11 -0300

    Update desafio6.js

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -8,10 +8,10 @@ db.movies.aggregate(
   {
     $group: {
       _id: "Vida longa e prospera",
-      maior_rating : { $max: "$imdb.rating" },
-      menor_rating : { $min: "$imdb.rating" },
-      media_rating : { $avg: "$imdb.rating" },
-      desvio_padrao : { $stdDevSamp: "$imdb.rating" },
+      maior_rating: { $max: "$imdb.rating" },
+      menor_rating: { $min: "$imdb.rating" },
+      media_rating: { $avg: "$imdb.rating" },
+      desvio_padrao: { $stdDevSamp: "$imdb.rating" },
     },
   },
   {
