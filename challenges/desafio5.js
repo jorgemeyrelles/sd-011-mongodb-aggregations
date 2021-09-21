e2b722f52adbb307aab0e9d0e9331d832a92c24e
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-09-21 19:58:03 -0300

    correçao desafio 5 e desafio 7

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -7,8 +7,10 @@ const favorites = [
 ];
 
 db.movies.aggregate([
-  { $match: { countries: { $eq: "USA" } } },
-  { $match: { "tomatoes.viewer.rating": { $gte: 3 } } },
+  { $match: {
+    countries: "USA",
+    "tomatoes.viewer.rating": { $gte: 3 },
+    cast: { $in: favorites } } },
   { $addFields: { num_favs: { $size: { $setIntersection: [favorites, "$cast"] } } } },
   { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
   { $project: { _id: 0, title: 1 } },
