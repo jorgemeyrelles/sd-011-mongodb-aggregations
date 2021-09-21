2fd5bebc95de01761f3b721b88f3b29ae31f5b5b
Hamander <1hamander@gmail.com>
2021-09-21 16:56:50 -0300

    desafio 7 completo

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,8 +1,28 @@
-use("aggregations");
 db.movies.aggregate([
   {
     $match: {
-      awards: { $exists: true, $regex: /won/ },
-    },
+      languages: { $eq: "English" },
+    }
+  },
+  { $unwind: "$cast" },
+  {
+    $group: {
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      mediaIMDB: { $avg: "$imdb.rating" },
+    }
+  },
+  {
+    $project: {
+      _id: 1,
+      numeroFilmes: 1,
+      mediaIMDB: { $round: ["$mediaIMDB", 1] },
+    }
+  },
+  {
+    $sort: {
+      numeroFilmes: -1,
+      _id: -1,
+    }
   },
 ]);
