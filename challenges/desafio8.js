835ac4409cf074f133784830854f773405154298
Filizzola <juliofilizzola@hotmail.com>
2021-09-21 08:09:07 -0300

    desafio 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,20 @@
+// use("aggregations");
+
+db.air_alliances.aggregate([
+  {
+    $lookup: {
+      from: 'air_routes',
+      localField: 'airlines',
+      foreignField: 'airline.name',
+      as: 'airplaneMatch'
+    }
+  }, 
+  { $unwind: "$airplaneMatch"},
+  { $match: { "airplaneMatch.airplane": { $in: ["747", "380"] } } },
+  { $group: {
+    _id: "$name",
+    totalRotas: { $sum: 1 }
+  } },
+  { $sort: { totalRotas: -1 } },
+  { $limit: 1 }
+]);
