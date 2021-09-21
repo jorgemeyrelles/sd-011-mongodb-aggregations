9f1098bf8489e128ccebd7cf61d94a12b4317d25
rodrigoruan <rodrigopython16@gmail.com>
2021-09-21 14:47:27 -0300

    Added exercise 2

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,11 +1,10 @@
-db.movies.aggregate({
-  $match: {
-    "imdb.rating": { $gte: 7 },
-    genres: { $nin: ["Crime", "Horror"] },
-    rated: { $in: ["PG", "G"] },
-    $and: [
-      { languages: { $eq: "English" } },
-      { languages: { $eq: "Spanish" } },
-    ],
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
+    },
   },
-});
+]);
