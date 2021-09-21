99d20e8f1b6babb0c2aee9961f7f3f3c4ce32938
Filizzola <juliofilizzola@hotmail.com>
2021-09-21 08:13:05 -0300

    fix desafio 10 | indentação

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -6,19 +6,18 @@ db.trips.aggregate(
       _id: "$usertype",
       duracaMedia: {
         $avg: {
-          $subtract: ["$stopTime", "$startTime"]
-        }
-      }
-    }
+          $subtract: ["$stopTime", "$startTime"],
+        },
+      },
+    },
   },
   {
     $sort: { duracaoMedia: 1 },
   },
   {
-  $project: {
-    _id: 0,
-    tipo: "$_id",
-    duracaMedia: { $round: [{ $divide: ["$duracaMedia", 60*60*1000]}, 2] },
-    _id: 0,
-  } }
-)
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaMedia: { $round: [{ $divide: ["$duracaMedia", 60 * 60 * 1000] }, 2] },
+    } },
+);
