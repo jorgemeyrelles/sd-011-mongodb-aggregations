09c5d24b41cb327c6c6f74e29a97c10b8d1a2f62
Hamander <1hamander@gmail.com>
2021-09-21 17:50:29 -0300

    Lint Fix

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -2,23 +2,23 @@ db.trips.aggregate([
   {
     $match: {
       birthYear: { $exists: true, $ne: "" },
-    }
+    },
   },
-  { 
-    $addFields: { birthYear: { $toInt:  "$birthYear" } } 
+  {
+    $addFields: { birthYear: { $toInt: "$birthYear" } },
   },
   {
     $group: {
       _id: null,
       maiorAnoNascimento: { $max: "$birthYear" },
       menorAnoNascimento: { $min: "$birthYear" },
-    }
+    },
   },
   {
     $project: {
       _id: 0,
       maiorAnoNascimento: "$maiorAnoNascimento",
       menorAnoNascimento: "$menorAnoNascimento",
-    }
-  }
+    },
+  },
 ]);
