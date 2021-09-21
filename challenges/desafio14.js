057e2ea7e65fe541c0b5e6bf78191e079078822d
IsleneGomes <islenegomes@gmail.com>
2021-09-21 20:56:49 -0300

    Desafio 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,29 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      duracao: {
+        $abs: {
+          $divide: [
+            { $subtract: ["$stopTime", "$startTime"] },
+            60 * 1000,
+          ],
+        } },
+    } },
+  {
+    $group: {
+      _id: "$bikeid",
+      duracao: { $avg: "$duracao" },
+    },
+  },
+  {
+    $sort: { duracao: -1 },
+  },
+  { $limit: 5 },
+  {
+    $project: {
+      bikeId: "$_id",
+      duracaoMedia: { $ceil: "$duracao" },
+      _id: 0,
+    },
+  },
+]);
