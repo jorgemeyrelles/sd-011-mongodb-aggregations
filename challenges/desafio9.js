8428796943cc16f4effbc3c4a075facae72f9e02
rodolforezende <rodolforrcont@gmail.com>
2021-09-21 16:55:52 -0300

    Desafio 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,21 @@
+db.trips.aggregate(
+  {
+    $match: { birthYear: { $exists: true, $ne: "" } },
+  }, {
+    $addFields: { birthYear: { $toInt: "$birthYear" } },
+  },
+  {
+    $group: {
+      maiorAnoNascimento: { $max: "$birthYear" },
+      menorAnoNascimento: { $min: "$birthYear" },
+      _id: null,
+    },
+  },
+  {
+    $project: {
+      maiorAnoNascimento: "$maiorAnoNascimento",
+      menorAnoNascimento: "$menorAnoNascimento",
+      _id: 0,
+    },
+  },
+);
