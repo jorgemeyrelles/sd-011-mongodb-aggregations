ed4f1b962c1e4dc2675172236f84245128a00de4
Filizzola <juliofilizzola@hotmail.com>
2021-09-21 08:18:10 -0300

    fix desafio 11 | indentação

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -2,14 +2,14 @@
 
 db.trips.aggregate([
   { $group: {
-    _id: { $dayOfWeek: "$startTime"},
-    total: { $sum: 1 }
-  }},
-  { $sort: { total: -1 }},
+    _id: { $dayOfWeek: "$startTime" },
+    total: { $sum: 1 },
+  } },
+  { $sort: { total: -1 } },
   { $limit: 1 },
   { $project: {
-    diaDaSemana: "$_id", 
+    diaDaSemana: "$_id",
     total: "$total",
     _id: 0,
-  } }
+  } },
 ]);
