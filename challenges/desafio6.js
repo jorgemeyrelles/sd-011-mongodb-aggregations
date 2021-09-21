558ad41e05edd2d67625eb36771447d2876d02d3
Hugosomers <hugosomersla@gmail.com>
2021-09-21 17:04:20 -0300

    Fix LINT

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,17 +1,17 @@
 db.movies.aggregate([
-  { $match: { awards: { $regex: /won \d+ oscar/i} } },
-  { $group: { 
-      _id: null,
-      maiorValor: { $max: "$imdb.rating" },
-      menorValor: { $min: "$imdb.rating" },
-      mediaValor: { $avg: "$imdb.rating" },
-      desvioPadrao: { $stdDevSamp: "$imdb.rating" },
-  }},
-  { $project: { 
+  { $match: { awards: { $regex: /won \d+ oscar/i } } },
+  { $group: {
+    _id: null,
+    maiorValor: { $max: "$imdb.rating" },
+    menorValor: { $min: "$imdb.rating" },
+    mediaValor: { $avg: "$imdb.rating" },
+    desvioPadrao: { $stdDevSamp: "$imdb.rating" },
+  } },
+  { $project: {
     _id: 0,
     maior_rating: "$maiorValor",
     menor_rating: "$menorValor",
     media_rating: { $round: ["$mediaValor", 1] },
     desvio_padrao: { $round: ["$desvioPadrao", 1] },
-  } }
+  } },
 ]);
