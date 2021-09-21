e678eb5c427287fc7fde96bef93852ed12c26f62
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-09-21 19:14:49 -0300

    req 8

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,3 +1,39 @@
-db.air_alliances.aggregate({
-  
-})
+db.air_alliances.aggregate([
+  {
+    $unwind: "$airlines",
+  },
+  {
+    $lookup: {
+      from: "air_routes",
+      localField: "airlines",
+      foreignField: "airline.name",
+      as: "alliance_routes",
+    },
+  },
+  {
+    $unwind: "$alliance_routes",
+  },
+  {
+    $match: {
+      "alliance_routes.airplane": { $in: ["747", "380"] },
+    },
+  },
+  {
+    $group: {
+      _id: "$name",
+      total_de_rotas: { $sum: 1 },
+    },
+  },
+  {
+    $sort: { total_de_rotas: -1 },
+  },
+  {
+    $project: {
+      _id: "$_id",
+      totalRotas: "$total_de_rotas",
+    },
+  },
+  {
+    $limit: 1,
+  },
+]);
