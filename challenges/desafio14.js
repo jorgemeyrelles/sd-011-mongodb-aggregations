bfb80db7a46e1e515897d0ac217c738ff37a3df6
LeoFuna <leo.funa@gmail.com>
2021-09-21 14:33:33 -0300

    Desafio 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,21 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$bikeid",
+      duracaoMediaEmMinutos: { $avg: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60 * 1000] } },
+    },
+  },
+  {
+    $sort: { duracaoMediaEmMinutos: -1 },
+  },
+  {
+    $limit: 5,
+  },
+  {
+    $project: {
+      _id: 0,
+      bikeId: "$_id",
+      duracaoMedia: { $ceil: "$duracaoMediaEmMinutos" },
+    },
+  },
+]);
