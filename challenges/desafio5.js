e552ab68d8f0f926a9d85a4e627c412c1286f20c
menezesrenan7 <renan_menezes07@hotmail.com>
2021-09-21 16:09:02 -0300

    Resoluçao do requisito 5.

---
+++ challenges/desafio5.js
@@ -0,0 +1,15 @@
+db.movies.aggregate([
+  { $match: { countries: "USA" } },
+  { $match: { "tomatoes.viewer.rating": { $gte: 3 } } },
+  { $match: { cast: { $exists: true } } },
+  { $addFields: { num_favs: { $size: { $setIntersection:
+    [
+      ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"],
+      "$cast",
+    ],
+  } } } },
+  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
+  { $limit: 25 },
+  { $skip: 24 },
+  { $project: { _id: 0, title: "$title" } },
+]);
