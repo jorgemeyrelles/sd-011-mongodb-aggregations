8d34857565bfe3b4a0b29979813de433ab521883
Josan Johnata <josanjohnata@gmail.com>
2021-09-21 20:19:47 -0300

    Corrigi desafio 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,6 +1,13 @@
-// db.trips.aggregate([
-//   { $group: { _id: { $dayOfWeek: "$startTime" }, total: { $sum: 1 } } },
-//   { $sort: { total: -1 } },
-//   { $project: { _id: false, diaDaSemana: "$_id", total: "$total" } },
-//   { $limit: 1 },
-// ]);
+db.trips.aggregate([
+  { $group: {
+    _id: { $dayOfWeek: "$startTime" },
+    total: { $sum: 1 },
+  } },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+  { $project: {
+    _id: 0,
+    diaDaSemana: "$_id",
+    total: "$total",
+  } },
+]);
