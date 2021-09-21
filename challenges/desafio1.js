371caf4d02d0b96e0a09210f019de1d0cafc1a7e
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-09-21 14:57:42 -0300

    desafio 1

---
+++ challenges/desafio1.js
@@ -0,0 +1,5 @@
+db.movies.aggregate([{ $match: { languages: { $all: ["English", "Spanish"] } } },
+  { $match: { rated: { $in: ["PG", "G"] } } },
+  { $match: { genres: { $nin: ["Crime", "Horror"] } } },
+  { $match: { "imdb.rating": { $gte: 7 } } },
+  { $count: "movies" }]);
