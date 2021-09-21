513bc759c5a960aa1b8a2791dd175bd657ad3cd1
PH Assis <pieit@live.com>
2021-09-21 18:09:00 -0300

    Challenge 12 ready

---
+++ challenges/desafio12.js
@@ -0,0 +1,18 @@
+// use("aggregations");
+db.trips.aggregate([
+  {
+    $set: {
+      diaDaSemana: { $dayOfWeek: "$startTime" },
+    },
+  },
+  { $match: { diaDaSemana: 5 } },
+  {
+    $group: {
+      _id: "$startStationName",
+      total: { $sum: 1 },
+    },
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+  { $project: { nomeEstacao: "$_id", _id: 0, total: "$total" } },
+]);
