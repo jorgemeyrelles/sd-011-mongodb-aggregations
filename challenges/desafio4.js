2de426ecf2d1e62a2a629e18f6417f69bda5aa83
Hamander <1hamander@gmail.com>
2021-09-21 17:49:11 -0300

    desafio 9 completo

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -5,12 +5,19 @@ db.movies.aggregate([
     },
   },
   {
-    $match: { title_split: { $size: 1 } },
+    $match: {
+      title_split: { $size: 1 },
+    },
   },
   {
-    $sort: { title: 1 },
+    $sort: {
+      title_split: 1,
+    },
   },
   {
-    $project: { title_split: 1, _id: 0 },
+    $project: {
+      _id: 0,
+      title_split: 1,
+    },
   },
 ]);
