685091e5fa591403307b0e23496bfb19fd0405b1
Hamander <1hamander@gmail.com>
2021-09-21 18:13:31 -0300

    Desafio 10 completo

---
+++ challenges/desafio10.js
@@ -0,0 +1,22 @@
+db.trips.aggregate(
+  {
+    $group: {
+      _id: "$usertype",
+      duracaMedia: {
+        $avg: {
+          $subtract: ["$stopTime", "$startTime"]
+        }
+      }
+    }
+  },
+  {
+    $sort: { duracaoMedia: 1 },
+  },
+  {
+  $project: {
+    _id: 0,
+    tipo: "$_id",
+    duracaMedia: { $round: [{ $divide: ["$duracaMedia", 60*60*1000]}, 2] },
+    _id: 0,
+  } }
+)
