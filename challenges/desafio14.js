053aefb00b9f7867842ec1b76c4ca370c192ca5d
Filizzola <juliofilizzola@hotmail.com>
2021-09-21 15:14:26 -0300

    fix desafio 141 | indentação

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -5,27 +5,27 @@ db.trips.aggregate(
     $addFields: {
       tempoMedio: {
         $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60000],
-      }
-    }
+      },
+    },
   },
   {
     $group: {
       _id: "$bikeid",
-      duracaoMedia: { $avg: "$tempoMedio" } 
-    }
+      duracaoMedia: { $avg: "$tempoMedio" },
+    },
   },
   {
     $sort: { duracaoMedia: -1 },
   },
-  { 
-    $limit: 5
+  {
+    $limit: 5,
   },
   {
     $project: {
       _id: 0,
-      bikeId: "$_id", 
-      duracaoMedia: { $ceil: "$duracaoMedia"}
-    }
-  }
+      bikeId: "$_id",
+      duracaoMedia: { $ceil: "$duracaoMedia" },
+    },
+  },
 
-)
+);
