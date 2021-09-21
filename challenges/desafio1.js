b06a5b5a0aff152cce55e6ecf8e11259272163d3
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-09-21 14:45:57 -0300

    Desafio 1

---
+++ challenges/desafio1.js
@@ -0,0 +1,4 @@
+db.movies.aggregate([{ $match: { "imdb.rating": { $gte: 7 },
+  genres: { $nin: ["Crime", "Horror"] },
+  $or: [{ rated: "PG" }, { rated: "G" }],
+  $and: [{ languages: "English" }, { languages: "Spanish" }] } }, { $limit: 41 }]);
