b798548718ad3e2718f896ab68c800784a246b2a
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-09-21 19:17:32 -0300

    requisitos 4 e 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,25 @@
+const favoriteA = [
+  "Sandra Bullock",
+  "Tom Hanks",
+  "Julia Roberts",
+  "Kevin Spacey",
+  "George Clooney",
+];
+db.movies.aggregate([
+  { $match: {
+    countries: "USA",
+    "tomatoes.viewer.rating": { $gte: 3 },
+    cast: { $in: favoriteA },
+  } },
+  { $addFields: {
+    num_favs: { $size: { $setIntersection: ["$cast", favoriteA] } },
+  } },
+  { $sort: {
+    num_favs: -1,
+    "tomatoes.viewer.rating": -1,
+    title: -1,
+  } },
+  { $project: { _id: 0, title: 1 } },
+  { $skip: 24 },
+  { $limit: 1 },
+]);
