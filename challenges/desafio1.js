65045523f7c1e908f32f74100fee529241270088
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-09-21 15:20:33 -0300

    desafios 1 a 7

---
+++ challenges/desafio1.js
@@ -0,0 +1,10 @@
+db.movies.aggregate([
+  { $match:
+    { $and: [
+      { "imdb.rating": { $gte: 7 } },
+      { genres: { $not: { $elemMatch: { $in: ["Crime", "Horror"] } } } },
+      { rated: { $in: ["G", "PG"] } },
+      { languages: { $all: ["English", "Spanish"] } },
+    ] },
+  },
+]);
---
+++ challenges/desafio5.js
@@ -0,0 +1,19 @@
+const favActors = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"];
+
+db.movies.aggregate([
+  { $match: {
+    $and: [
+      { countries: "USA" },
+      { "tomatoes.viewer.rating": { $gte: 3 } },
+    ],
+  } },
+  { $addFields: {
+    num_favs: {
+      $cond: { if: { $isArray: "$cast" }, then: { $size: { $setIntersection: [favActors, "$cast"] } }, else: -1 } },
+  },
+  },
+  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
+  { $skip: 24 },
+  { $limit: 1 },
+  { $project: { _id: 0, title: 1 } },
+]);
