3e9e2551f8cce813fcfa886dc82e8049a981ba37
PH Assis <pieit@live.com>
2021-09-21 10:27:05 -0300

    Added challenge 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,16 @@
+// use("aggregations");
+
+db.movies.aggregate([
+  {
+    $project: {
+      title_split: { $split: ["$title", " "] },
+      _id: 0,
+    },
+  },
+  {
+    $match: {
+      title_split: { $size: 1 },
+    },
+  },
+  { $sort: { title_split: 1 } },
+]);
