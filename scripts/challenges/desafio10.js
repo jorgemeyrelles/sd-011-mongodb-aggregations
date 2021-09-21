94b7853553c64545e864badf501072fe233aa3cb
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-09-21 16:25:12 -0300

    Ex 1.

---
+++ scripts/challenges/desafio10.js
@@ -0,0 +1,10 @@
+// Desafio 10
+// Encontre a duração média de viagens por tipo de usuário.
+// Exiba o valor em horas com apenas duas casas decimais
+// Exiba a média de viagens ordenada de forma crescente.
+// Para arredondar a média use o $round.
+
+// O resultado da sua query deve ter exatamente o seguinte formato (incluindo a ordem dos campos):
+
+// { "tipo" : <tipo>, "duracaoMedia" : <duracaoMedia> }
+// // ...
---
+++ scripts/challenges/desafio3.js
@@ -0,0 +1,10 @@
+// Desafio 3
+// Agora que você tem os campos essenciais,
+// aplique mais um estágio na pipeline do desafio anterior que atenda a seguinte demanda:
+
+// Retorne esses filmes ordenados por ano e nota IMDB de
+// forma decrescente e título por ordem alfabética.
+// O resultado da sua query deve ter exatamente o seguinte formato (incluindo a ordem dos campos):
+
+// { "titulo" : "McFarland, USA", "avaliado" : "PG", "notaIMDB"
+//  : 7.5, "votosIMDB" : 14091, "ano" : 2015 }
