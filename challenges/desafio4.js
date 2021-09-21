421306d168e2732b712bb38a748e6c883e49c3cb
marlon307 <marlon-ramosb@hotmail.com>
2021-09-21 17:34:26 -0300

    Requesito 4 update

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -11,12 +11,12 @@ db.movies.aggregate([{
     },
   },
 }, {
-  $project: {
-    _id: 0,
-    title: 1,
+  $sort: {
+    title_split: 1,
   },
 }, {
-  $sort: {
-    title: 1,
+  $project: {
+    _id: 0,
+    title_split: 1,
   },
 }]);
