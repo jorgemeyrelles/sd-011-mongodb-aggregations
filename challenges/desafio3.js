f9b589c2c0f9328b04012894d20116eacebab76c
OldRoge <rogeriomrjr@icloud.com>
2021-09-22 16:51:30 -0300

    Done requirement 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,48 @@
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        {
+          "imdb.rating": {
+            $gte: 7,
+          },
+        },
+        {
+          $nor: [
+            { genres: "Crime" },
+            { genres: "Horror" },
+          ],
+        },
+        {
+          $or: [
+            { rated: "PG" },
+            { rated: "G" },
+          ],
+        },
+        {
+          $and: [
+            { languages: "English" },
+            { languages: "Spanish" },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    $project: {
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+      _id: 0,
+    },
+  },
+  {
+    $sort: {
+      ano: -1,
+      notaIMDB: -1,
+      titulo: 1,
+    },
+  },
+]);
