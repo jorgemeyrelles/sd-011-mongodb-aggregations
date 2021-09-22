8072e099e4b5b70ec52210152fc8c6d33fc9a997
jorgemeyrelles <jotaengpuc@gmail.com>
2021-09-22 12:48:42 -0300

    fix: desafio8

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,27 +1,3 @@
-db.air_alliances.aggregate([
-  {
-    $unwind: "$airlines",
-  },
-  {
-    $lookup: {
-      from: "air_routes",
-      localField: "airlines",
-      foreignField: "airline.name",
-      as: "departures",
-    },
-  },
-  {
-    $unwind: "$departures",
-  },
-  {
-    $match: { "departures.airplane": { $in: ["747", "380"] } },
-  },
-  {
-    $group: { _id: "$departures.airline.name", totalRotas: { $sum: 1 } },
-  },
-  { $sort: { totalRotas: -1 } },
-  { $limit: 1 },
-]);
 // db.air_alliances.aggregate([
 //   {
 //     $unwind: "$airlines",
@@ -41,8 +17,32 @@ db.air_alliances.aggregate([
 //     $match: { "departures.airplane": { $in: ["747", "380"] } },
 //   },
 //   {
-//     $group: { _id: "$name", totalRotas: { $sum: 1 } },
+//     $group: { _id: "$departures.airline.name", totalRotas: { $sum: 1 } },
 //   },
 //   { $sort: { totalRotas: -1 } },
 //   { $limit: 1 },
 // ]);
+db.air_alliances.aggregate([
+  {
+    $unwind: "$airlines",
+  },
+  {
+    $lookup: {
+      from: "air_routes",
+      localField: "airlines",
+      foreignField: "airline.name",
+      as: "departures",
+    },
+  },
+  {
+    $unwind: "$departures",
+  },
+  {
+    $match: { "departures.airplane": { $in: ["747", "380"] } },
+  },
+  {
+    $group: { _id: "$name", totalRotas: { $sum: 1 } },
+  },
+  { $sort: { totalRotas: -1 } },
+  { $limit: 1 },
+]);
