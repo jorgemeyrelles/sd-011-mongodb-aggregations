12ad9b1f88fc42584397e6b0fac8f41a6ca051e4
gabiazevedo <gabiazevedoms@gmail.com>
2021-09-22 15:09:52 -0300

    Finaliza desafio 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,26 @@
+db.trips.aggregate([
+  {
+    $match: {
+      birthYear: { $exists: true },
+    },
+  },
+  {
+    $match: {
+      birthYear: { $ne: "" },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maiorAnoNascimento: { $max: "$birthYear" },
+      menorAnoNascimento: { $min: "$birthYear" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      maiorAnoNascimento: { $toInt: "$maiorAnoNascimento" },
+      menorAnoNascimento: { $toInt: "$menorAnoNascimento" },
+    },
+  },
+]);
