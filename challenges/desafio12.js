2ed9396005bc5ae8c36a28824525804703662f39
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-09-22 15:55:38 -0300

    Correcoes

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,6 +1,8 @@
+use("aggregations");
+
 db.trips.aggregate([
   { $group: { _id: { diaDaSemana: { $dayOfWeek: "$startTime" }, nomeEstacao: "$startStationName" }, total: { $sum: 1 } } },
   { $sort: { total: -1 } },
   { $limit: 1 },
-  { $project: { _id: 0, nomeEstacao: "$_id.nomeEstacao", total: 1 } },
+  { $project: { _id: 0, nomeEstacao: "$_id.nomeEstacao", total: "$total" } },
 ]);
