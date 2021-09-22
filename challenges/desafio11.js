fd0eaddafd85c2f6fe128c9ffa7829cc64776778
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-09-22 14:32:33 -0300

    Desafio 12

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,6 +1,8 @@
+use("aggregations");
+
 db.trips.aggregate([
   { $group: { _id: { $dayOfWeek: "$startTime" }, total: { $sum: 1 } } },
   { $sort: { total: -1 } },
   { $limit: 1 },
-  { $project: { _id: 0, diaDaSemana: "$_id", total: 1 } },
+  { $project: { diaDaSemana: "$_id", total: "$total", _id: 0  } },
 ]);
