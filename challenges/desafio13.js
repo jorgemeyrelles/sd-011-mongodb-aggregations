ac3c55633a7e5fad14151347fde5198a6618bf1e
Tales Coelho dos Santos <tales-coelho@live.com>
2021-09-22 15:34:30 -0300

    Create desafio13.js

---
+++ challenges/desafio13.js
@@ -0,0 +1,26 @@
+db.trips.aggregate(
+  {
+    $match: {
+      startTime: {
+        $gte: ISODate("2016-03-10T00:00:00.0Z"),
+        $lte: ISODate("2016-03-11T00:00:00.0Z"),
+      },
+    },
+  },
+  { $group: {
+      _id: "Vida Loga e prospera",
+      duracaoMediaEmMinutos: {
+        $avg: {
+          $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60000],
+        },
+      },
+   }},
+   {
+    $project: {
+      duracaoMediaEmMinutos: {
+        $ceil: "$duracaoMediaEmMinutos",
+      },
+      _id: 0,
+    },
+  },
+);
