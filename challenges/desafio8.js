c77673b4749b6573560e71ad37c9e0ae0dfa18b2
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-09-22 16:00:10 -0300

    req8 ok

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -2,26 +2,31 @@ db.air_alliances.aggregate([
   {
     $lookup: {
       from: "air_routes",
-      let: {
-        alliances_name: "$name",
-        alliances_airlines_name: "$airlines",
-      },
-      pipeline: [
-        {
-          $match: {
-            airplane: {
-              $in: ["747", "380"],
-            },
-          },
-        },
-      ],
-      as: "totalRotas",
+      localField: "airlines",
+      foreignField: "airline.name",
+      as: "rotas",
     },
   },
-  { $unwind: "$totalRotas" },
+  { $unwind: "$rotas" },
+  {
+    $match: {
+      "rotas.airplane": {
+        $in: ["747", "380"],
+      },
+    },
+  },
   {
     $group: {
-      rotas: { $sum: 1 },
+      _id: "$name",
+      totalRotas: { $sum: 1 },
     },
+  },
+  {
+    $sort: {
+      totalRotas: -1,
+    },
+  },
+  {
+    $limit: 1,
   },
 ]);
