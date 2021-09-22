880b86996471eaea30d2712fc5eaa1ebb727e63a
Amandha W. Barok <amandha.barok@gmail.com>
2021-09-22 16:19:59 -0700

    11th done

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,23 +2,23 @@ db.trips.aggregate([
   {
     $addFields: {
       total: {
-        $divide:[
-          {$subtract: [ "$stopTime", "$startTime" ]},
-          1000 * 60 * 60
-        ]
-      }
-    }
+        $divide: [
+          { $subtract: ["$stopTime", "$startTime"] },
+          1000 * 60 * 60,
+        ],
+      },
+    },
   },
   {
     $group: {
       _id: "$usertype",
-      duracaoMediaInt: { $avg: "$total"}
-    }
+      duracaoMediaInt: { $avg: "$total" },
+    },
   },
   {
     $project: {
       usertype: 1,
-      duracaoMedia: { $round: ["$duracaoMediaInt", 2]}
-    }
-  }
+      duracaoMedia: { $round: ["$duracaoMediaInt", 2] },
+    },
+  },
 ]);
