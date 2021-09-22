e420f0e6deb8d5705dfbe3e7664a9fddd3d68be8
Vinicius <viniciusfod@gmail.com>
2021-09-22 05:31:28 -0300

    fixreq1-2-3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,10 +1,12 @@
 db.movies.aggregate([
-  { $match: {
-    "imdb.rating": { $gte: 7 },
-    genres: { $nin: ["Crime", "Horror"] },
-    rated: { $in: ["PG", "G"] },
-    languages: { $in: ["English", "Spanish"] },
-  } },
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
+    },
+  },
   {
     $project: {
       _id: 0,
@@ -15,5 +17,11 @@ db.movies.aggregate([
       ano: "$year",
     },
   },
-  { $sort: { ano: -1, notaIMDB: -1, titulo: 1 } },
+  {
+    $sort: {
+      ano: -1,
+      notaIMDB: -1,
+      titulo: 1,
+    },
+  },
 ]);
