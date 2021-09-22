67e8bc8b8279ac3a53f9bc3cfc23e12b5245236d
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-09-22 14:57:33 -0300

    feito requisito 5 indo pro 6

---
+++ challenges/desafio3.js
@@ -0,0 +1,30 @@
+db.movies.aggregate([
+  {
+    $match:
+      {
+        $and:
+        [
+          { genres: { $nin: ["Crime", "Horror"] }  },
+          { "imdb.rating": { $gte: 7 } },
+          { rated: { $in: ["PG", "G"] } },
+          { languages: { $all: ["English", "Spanish"] } },
+        ]
+      },
+  },
+  {
+    $project: {
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$ano"
+    }
+  },
+  {
+    $sort: {
+      titulo: 1,
+      notaIMDB: -1,
+      ano: -1,
+    }
+  }
+]);
---
+++ challenges/desafio5.js
@@ -0,0 +1,10 @@
+let preferidas = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"];
+
+db.movies.aggregate([
+  { $match: { countries: "USA" }, "tomatoes.viewer.rating": { $gte: 3 }, cast: { $in: preferidas }  },
+  { $project: { num_favs: { $size: { $setIntersection: ["$cast", preferidas] } }  } },
+  { $project: { title: 1, _id: 0 } },
+  { $skip: 24 },
+  { $limit: 1 },
+  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
+]);
