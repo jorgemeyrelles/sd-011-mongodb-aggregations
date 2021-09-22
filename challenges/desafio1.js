7a391fd4e797ff035537c462e9643fbc37ca4003
OldRoge <rogeriomrjr@icloud.com>
2021-09-22 15:28:11 -0300

    Done requirement 1

---
+++ challenges/desafio1.js
@@ -0,0 +1,31 @@
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        {
+          "imdb.rating": {
+            $gte: 7,
+          },
+        },
+        {
+          $nor: [
+            { genres: "Crime" },
+            { genres: "Horror" },
+          ],
+        },
+        {
+          $or: [
+            { rated: "PG" },
+            { rated: "G" },
+          ],
+        },
+        {
+          $and: [
+            { languages: "English" },
+            { languages: "Spanish" },
+          ],
+        },
+      ],
+    },
+  },
+]);
