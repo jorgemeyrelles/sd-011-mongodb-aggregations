16b2cc6fde84b5b4ecb7ce9630d5bbd951d28f2e
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-09-22 11:38:33 -0300

    correçao desafio 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,42 +1,74 @@
-const favActors = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"];
+/*
+  source: {
+    mongodbDocumentation: {
+      let: https://docs.mongodb.com/manual/reference/operator/aggregation/let/,
+      setIntersection: https://docs.mongodb.com/manual/reference/operator/aggregation/setIntersection/,
+    },
+    pullRequests: {
+      anaClara: https://github.com/tryber/sd-011-mongodb-aggregations/blob/anaclarabck-mongodb-aggregations/challenges/desafio5.js
+      richard: https://github.com/tryber/sd-011-mongodb-aggregations/blob/richard-freitas-mongodb-aggregations/challenges/desafio5.js
+      inacio: https://github.com/tryber/sd-011-mongodb-aggregations/blob/heyset-aggregations-project/challenges/desafio5.js
+    }
+  }
+*/
 
 db.movies.aggregate([
   {
     $match: {
-      countries: "USA",
+      cast: {
+        $exists: true,
+        $in: [
+          "Sandra Bullock",
+          "Tom Hanks",
+          "Julia Roberts",
+          "Kevin Spacey",
+          "George Clooney",
+        ],
+      },
+      countries: { $in: ["USA"] },
       "tomatoes.viewer.rating": { $gte: 3 },
-      cast: { $exists: true },
-    },
-  },
-  {
-    $addFields: {
-      num_favs_array: {
-        $setIntersection: ["$cast", favActors],
-      },
-    },
-  },
-  {
-    $match: {
-      num_favs_array: {
-        $setIntersection: ["$cast", favActors],
-      },
-    },
-  },
-  {
-    $addFields: {
-      num_favs: {
-        $size: "$num_favs_array",
-      },
     },
   },
   {
     $project: {
-      title: 1,
       _id: 0,
+      title: 1,
       cast: 1,
+      rating: "$tomatoes.viewer.rating",
+      num_favs: {
+        $size: {
+          $setIntersection: [
+            [
+              "Sandra Bullock",
+              "Tom Hanks",
+              "Julia Roberts",
+              "Kevin Spacey",
+              "George Clooney",
+            ],
+            "$cast",
+          ],
+        },
+      },
     },
   },
   {
-    $skip: 25,
+    $sort: {
+      num_favs: -1,
+      rating: -1,
+      title: -1,
+    },
+  },
+  {
+    $skip: 24,
+  },
+  {
+    $limit: 1,
+  },
+  {
+    $project: {
+      _id: 0,
+      title: 1,
+      rating: "$tomatoes.viewer.rating",
+    },
   },
 ]);
