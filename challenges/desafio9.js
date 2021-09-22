f42dc4dfba7e0a74b38c19d3e6ad15e98b67614a
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-09-22 18:40:34 -0300

    desafio 9 - completo

---
+++ challenges/desafio9.js
@@ -0,0 +1,30 @@
+db.trips.aggregate([
+  {
+    $match:
+    {
+      birthYear: { $exists: true, $ne: "" },
+    },
+  },
+  {
+    $project:
+    {
+      birthYear: {
+        $toInt: "$birthYear",
+      },
+    },
+  },
+  {
+    $group:
+    {
+      _id: null,
+      maiorAnoNascimento: { $max: "$birthYear" },
+      menorAnoNascimento: { $min: "$birthYear" },
+    },
+  },
+  {
+    $project:
+    {
+      _id: 0,
+    },
+  },
+]);
