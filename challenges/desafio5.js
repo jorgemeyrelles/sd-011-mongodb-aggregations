6668756c160b6373ccaf3efcf0e19636491bcac1
Amandha W. Barok <amandha.barok@gmail.com>
2021-09-22 11:34:41 -0700

    fixing 5th and adding 6th

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -10,18 +10,25 @@ db.movies.aggregate([
           { cast: { $eq: "Kevin Spacey" } },
           { cast: { $eq: "George Clooney" } },
         ] },
+        { "tomatoes.viewer.rating": { $gte: 3 } },
       ],
     },
-
   },
   { $addFields: {
-    num_favs: { $add: ["$num_favs", 1] },
+    favs: { $setIntersection: ["$cast",
+      ["Sandra Bullock", "Tom Hanks",
+        "Julia Roberts", "Kevin Spacey", "George Clooney",
+      ]] },
+  } },
+  { $addFields: {
+    num_favs: { $size: "$favs" },
   } },
   { $sort: {
-    num_favs: 1,
-    "tomatoes.viewer.rating": 1,
-    title: 1,
+    num_favs: -1,
+    "tomatoes.viewer.rating": -1,
+    title: -1,
   } },
+  { $skip: 24 },
   { $limit: 1 },
   { $project: {
     _id: 0,
