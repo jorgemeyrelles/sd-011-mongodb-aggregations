e06fb016ac46e85b35837fa035ed504594a61434
guinhoanjos <iago.anjos@hotmail.com>
2021-09-22 14:43:35 -0300

    tirando os matchs duplicados

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -2,15 +2,8 @@ db.movies.aggregate([
   {
     $match: {
       "imdb.rating": { $gte: 7 },
-    },
-    $match: {
       $nor: [{ genres: "Crime" }, { genres: "Horror" }],
-    },
-    $match: {
-      $or: [{ rated: "PG" }, { rated: "G" }],
-    },
-    $match: {
-      $or: [{ languages: "English" }, { languages: "Spanish" }],
+      $or: [{ rated: "PG" }, { rated: "G" }, { languages: "English" }, { languages: "Spanish" }],
     },
   },
 ]);
