43fed46c5347f44ba213bc51df6973951c6b7957
Tales Coelho dos Santos <tales-coelho@live.com>
2021-09-22 14:41:23 -0300

    Create desafio9.js

---
+++ challenges/desafio9.js
@@ -0,0 +1,22 @@
+db.trips.aggregate( 
+  {
+    $match: { birthYear: { $exists: true, $ne: "" } },
+  },
+  {
+    $addFields: { birthYear: { $toInt:  "$birthYear" } },
+  },
+  {
+    $group: {
+      maiorAnoNascimento: { $max: "$birthYear" },
+      menorAnoNascimento: { $min: "$birthYear" },
+      _id: "Paz na vida",
+    },
+  },
+  {
+    $project: { 
+      maiorAnoNascimento: "$maiorAnoNascimento",
+      menorAnoNascimento: "$menorAnoNascimento",
+      _id: 0,
+    },
+  },
+);
