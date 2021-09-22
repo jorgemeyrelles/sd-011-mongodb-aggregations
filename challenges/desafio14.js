b7cd90680dd3957a97287970818459624cdb82e5
Tales Coelho dos Santos <tales-coelho@live.com>
2021-09-22 15:44:45 -0300

    Create desafio14.js

---
+++ challenges/desafio14.js
@@ -0,0 +1,28 @@
+db.trips.aggregate(
+  {
+    $addFields: {
+      tempoMedio: {
+        $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60000],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$bikeid",
+      duracaoMedia: { $avg: "$tempoMedio" },
+    },
+  },
+  {
+    $sort: { duracaoMedia: -1 },
+  },
+  {
+    $limit: 5,
+  },
+  {
+    $project: {
+      _id: 0,
+      bikeId: "$_id",
+      duracaoMedia: { $ceil: "$duracaoMedia" },
+    },
+  },
+);
