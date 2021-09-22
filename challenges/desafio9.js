e609f2ab8e699ab8382c018a9214bbd8938cd274
MatheusAraujoDev <matheusaraujo.dev@gmail.com>
2021-09-22 16:52:57 -0300

    [ADD] req 09

---
+++ challenges/desafio9.js
@@ -0,0 +1,24 @@
+db.trips.aggregate([
+  {
+    $match: {
+      birthYear: {
+        $exists: true, // filtra os que tem birthYear, e retira os campos vazios.
+        $ne: " ",
+      },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maiorAnoNascimento: { $max: { $toInt: "birthYear" } },
+      menorAnoNascimento: { $min: { $toInt: "birthYear" } },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      maiorAnoNascimento: 1,
+      menorAnoNascimento: 1,
+    },
+  },
+]);
