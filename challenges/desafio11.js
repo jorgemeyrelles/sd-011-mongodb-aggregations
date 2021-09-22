7acce833ea27a86e96fbe54b690592a6de3ee93a
gabiazevedo <gabiazevedoms@gmail.com>
2021-09-22 19:17:45 -0300

    Finaliza desafio 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,14 +1,18 @@
 db.trips.aggregate([
   {
     $group: {
-      _id: null,
-      diaDaSemana: { $max: { $dayOfWeek: "$startTime" } },
+      _id: { $dayOfWeek: "$startTime" },
       total: { $sum: 1 },
     },
   },
   {
     $project: {
       _id: 0,
+      diaDaSemana: "$_id",
+      total: "$total",
     },
+  },
+  {
+    $sort: { total: -1 },
   },
 ]);
