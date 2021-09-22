a32b90ff93f561b4c1ac16b87079ee28266f8f8f
IsaacMagno <isaacmagno98@gmail.com>
2021-09-22 15:52:22 -0300

    desafio1cor

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,7 +1,7 @@
 db.movies.aggregate(
   [{ $match: { $and: [{ "imdb.rating": { $gte: 7 } },
-             { genres: { $not: { $in: [ "Crime", "Horror" ] } } },
-             { $or: [{ rated: { $eq: "PG" } }, { rated: { $eq: "G" } }] },
-             { $and: [{ languages: { $eq: "English" } }, { languages: { $eq: "Spanish" } }] }] },
-            }],
+      { genres: { $not: { $in: ["Crime", "Horror"] } } },
+      { $or: [{ rated: { $eq: "PG" } }, { rated: { $eq: "G" } }] },
+      { $and: [{ languages: { $eq: "English" } }, { languages: { $eq: "Spanish" } }] }] },
+    }],
   );
