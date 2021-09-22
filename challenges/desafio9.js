be6d5beab31d456fece301a2f2326d37c96575ca
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-09-22 19:39:52 -0300

    corrige linter

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -6,7 +6,7 @@ Não considere documentos com valores vazios ("") e em que o campo não existe!
 
 Para este desafio utilize o operador $toInt para converter de string para valor inteiro. */
 db.trips.aggregate([
-  //https://stackoverflow.com/questions/4057196/how-do-you-query-for-is-not-null-in-mongo
+  // https://stackoverflow.com/questions/4057196/how-do-you-query-for-is-not-null-in-mongo
   { $match: { birthYear: { $nin: [""] } } },
   { $addFields: { intBirth: { $toInt: "$birthYear" } } },
   {
