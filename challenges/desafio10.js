2328b33fd2a1ad74f02c2023a7591f5f075350a8
Ghislaine <glenox79@gmail.com>
2021-09-22 19:27:43 -0300

    Desafio 10 -  adicioando

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -8,5 +8,7 @@ db.trips.aggregate([
   { $project: {
     _id: 0,
     tipo: "$_id",
-    duracaoMedia: { $round: ["$duracaoMedia", 2] } } },
+    duracaoMedia: { $round: ["$duracaoMedia", 2] },
+  } },
+  { $sort: { duracaoMedia: 1 } },
 ]);
