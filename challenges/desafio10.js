91c98aa875ce85f3248b4e86e47972d431212d54
Vinicius <viniciusfod@gmail.com>
2021-09-22 05:30:28 -0300

    fixreq10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,23 +1,25 @@
 db.trips.aggregate([
   {
+    $addFields: {
+      duracao: { $subtract: ["$stopTime", "$startTime"] },
+    },
+  },
+  {
     $group: {
       _id: "$usertype",
-      duracaoMedia: {
-        $avg: {
-          $subtract: ["$stopTime", "$startTime"],
-        },
-      },
+      duracaoMedia: { $avg: "$duracao" },
     },
   },
   {
     $project: {
       _id: 0,
       tipo: "$_id",
-      duracaoMedia: {
-        $round: [{
-          $divide: ["$duracaoMedia", 1000 * 60 * 60],
-        }, 2],
-      },
+      duracaoMedia: { $round: [{ $divide: ["$duracaoMedia", 3600000] }, 2] },
+    },
+  },
+  {
+    $sort: {
+      duracaoMedia: 1,
     },
   },
 ]);
