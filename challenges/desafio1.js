a52100a20e1fead8e11c2f634b1af2dae65c7ff9
guinhoanjos <iago.anjos@hotmail.com>
2021-09-22 15:15:20 -0300

    Update desafio1.js

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -4,8 +4,8 @@ db.movies.aggregate([
       $and: [
         { "imdb.rating": { $gte: 7 } },
         { $nor: [{ genres: "Crime" }, { genres: "Horror" }] },
-        { $or: [{ rated: "PG" }, { rated: "G" }, { languages: "English" }, { languages: "Spanish" }] },
+        { rated: { $in: ["PG", "G"] } },
+        { languages: { $all: ["English", "Spanish"] } },
       ],
-    },
-  },
+    } },
 ]);
