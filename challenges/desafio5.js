dc9c3e42a7f193b6773c6c0ecdd842b5341313a7
Marconi <marconieng@gmail.com>
2021-09-22 15:43:53 -0300

    Corrigindo desafio 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,23 +1,36 @@
 db.movies.aggregate([
-  { $match: { $and: [{ countries: { $all: ["USA"] } },
-    { "tomatoes.viewer.rating": { $gte: 3 } },
-    { cast: { $exists: true } }] } },
-  { $addFields:
   {
-    num_favs: {
-      $setIntersection: [
+    $match: {
+      $and:
         [
-          "Sandra Bullock",
-          "Tom Hanks",
-          "Julia Roberts",
-          "Kevin Spacey", "George Clooney",
+          { countries: { $all: ["USA"] } },
+          { "tomatoes.viewer.rating": { $gte: 3 } },
+          { cast: { $exists: true } },
         ],
-        "$cast",
-      ],
     },
   },
+  {
+    $addFields:
+    {
+      num_favs: {
+        $size:
+        {
+          $setIntersection: [
+            [
+              "Sandra Bullock",
+              "Tom Hanks",
+              "Julia Roberts",
+              "Kevin Spacey",
+              "George Clooney",
+            ],
+            "$cast",
+          ],
+        },
+
+      },
+    },
   },
+  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
   { $skip: 24 },
   { $limit: 1 },
-  { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
   { $project: { _id: 0, title: 1 } }]);
