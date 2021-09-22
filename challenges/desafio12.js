99f786238b2490a2f084e31faf9a95d0d9e17b67
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-09-22 11:32:12 -0300

    Desafio 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,14 +1,25 @@
 db.trips.aggregate([
-  { $addFields: { diaDaSemana: { $dayOfWeek: "$startTime" } } },
-  { $match: { diaDaSemana: 5 } },
-  { $group: { _id: "$startStationName", total: { $sum: 1 } } },
-  { $sort: { total: -1 } },
-  { $limit: 1 },
+  {
+    $group: {
+      _id: {
+        diaDaSemana: { $dayOfWeek: "$startTime" },
+        nomeEstacao: "$startStationName",
+      },
+      total: { $sum: 1 },
+    },
+  },
+  {
+    $sort: {
+      total: -1,
+    },
+  },
   {
     $project: {
       _id: 0,
-      nomeEstacao: "$_id",
-      total: "$total",
-    },
+      nomeEstacao: "$_id.nomeEstacao",
+      total: "$total" },
+  },
+  {
+    $limit: 1,
   },
 ]);
