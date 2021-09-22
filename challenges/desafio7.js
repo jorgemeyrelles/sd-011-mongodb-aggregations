7a78521baf38d133065cbf12faff84ea3acc6829
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-22 16:27:25 -0300

    Adicionando desafio7

---
+++ challenges/desafio7.js
@@ -0,0 +1,15 @@
+db.movies.aggregate([
+  {
+    $match: { languages: "English", cast: { $exists: true } },
+  },
+  { $unwind: "$cast" },
+  {
+    $group: {
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      mediaIMDB: { $avg: "$imdb.rating" },
+    },
+  },
+  { $sort: { numeroFilmes: -1, _id: -1 } },
+  { $project: { numeroFilmes: 1, mediaIMDB: { $round: ["$mediaIMDB", 1] } } },
+]);
