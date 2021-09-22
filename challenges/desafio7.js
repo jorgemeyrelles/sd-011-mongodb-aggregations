91f2f160ae9dae32937a144a1d29fc3388581c80
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-09-22 16:51:38 -0300

    desafio 7 - renomenando a colunas e as colocando em ordem decrescente de documentos pelo número de filmes e nome do ator ou atriz

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -89,3 +89,42 @@ db.movies.aggregate([
     },
   },
 ]);
+
+// renomenando a colunas e as colocando em ordem decrescente de documentos pelo número de filmes e nome do ator ou atriz
+db.movies.aggregate([
+  {
+    $match: {
+      languages: { $all: ["English"] },
+    },
+  },
+  {
+    $unwind: "$cast",
+  },
+  {
+    $addFields: {
+      imdbRating: "$imdb.rating",
+    },
+  },
+  {
+    $group:
+    {
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      mediaIMDB: { $avg: "$imdbRating" },
+    },
+  },
+  {
+    $project:
+    {
+      numeroFilmes: 1,
+      mediaIMDB: { $round: ["$mediaIMDB", 1] },
+    },
+  },
+  {
+    $sort:
+    {
+      numeroFilmes: -1,
+      _id: -1,
+    },
+  },
+]);
