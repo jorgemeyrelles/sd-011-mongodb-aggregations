ab7e4970b0d45c44d83385314ee57ea3e878b2e1
Thiagofox <thiagofox00@gmail.com>
2021-09-21 21:34:38 -0300

    Desafios 7 finalizado

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -20,3 +20,33 @@ exatamente o seguinte formato (incluindo a ordem dos campos):
 
 { "_id" : "John Wayne", "numeroFilmes" : 107, "mediaIMDB" : 6.4 }
  */
+
+// use("aggregations");
+db.movies.aggregate([
+  {
+    $match: {
+      languages: { $all: ["English"] },
+    },
+  },
+
+  { $unwind: "$cast" },
+
+  { $group: {
+    _id: "$cast",
+    numeroFilmes: { $sum: 1 },
+    mediaIMDB: { $avg: "$imdb.rating" },
+  },
+  },
+  {
+    $project: {
+      _id: 1,
+      numeroFilmes: 1,
+      mediaIMDB: { $round: ["$mediaIMDB", 1] },
+    },
+  },
+  { $sort: {
+    numeroFilmes: -1,
+    _id: -1,
+  },
+  },
+]);
