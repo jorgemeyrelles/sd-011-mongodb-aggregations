2023da450d8a99d4215f367b1558f96b33a20843
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-09-22 14:59:50 -0300

    Corrige Lint Desafio 1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -2,5 +2,5 @@ db.movies.aggregate([
   { $match: { "imdb.rating": { $gte: 7 } } },
   { $match: { genres: { $nin: ["Crime", "Horror"] } } },
   { $match: { rated: { $in: ["PG", "G"] } } },
-  { $match: { languages: { $all: ["English", "Spanish"] } } }
+  { $match: { languages: { $all: ["English", "Spanish"] } } },
 ]);
