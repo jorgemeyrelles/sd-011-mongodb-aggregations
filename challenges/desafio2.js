bf54c4d932e5f1afb0fc474b2646053e134a72f7
Juliana Happatsch <jhappatsch@gmail.com>
2021-09-22 16:43:14 -0300

    req 2

---
+++ challenges/desafio2.js
@@ -0,0 +1,26 @@
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      $or: [{ rated: "PG" }, { rated: "G" }],
+      languages: { $all: ["English", "Spanish"] },
+    },
+  },
+  { $limit: 41 },
+  { $addFields: {
+    titulo: "$title",
+    avaliado: "$rated",
+    notaIMDB: "$imdb.rating",
+    votosIMDB: "$imdb.votes",
+    ano: "$year",
+  } },
+  { $project: {
+    _id: 0,
+    titulo: 1,
+    avaliado: 1,
+    notaIMDB: 1,
+    votosIMDB: 1,
+    ano: 1,
+  } },
+]);
