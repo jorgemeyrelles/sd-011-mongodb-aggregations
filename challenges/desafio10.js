340d5fd6e38de085e74f3d795574e88528cf1dcb
Dário Junior <dario.junior.rs@gmail.com>
2021-09-22 17:32:18 -0300

    tinha deixado resto de código no 7, agora está ok, e corrige o 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -24,38 +24,3 @@ db.trips.aggregate([
     },
   },
 ]);
-
-// db.trips.aggregate([
-//   {
-//     $group: {
-//       _id: "$usertype",
-//       duracaoMedia: {
-//         $avg: {
-//           $subtract: [
-//             {
-//               $sum: [
-//                 { "$multiply": [{ "$hour": "$stopTime" }, 3600] },
-//                 { "$multiply": [{ "$minute": "$stopTime" }, 60] },
-//                 { "$multiply": [{ "$second": "$stopTime" }, 1] },
-//               ]
-//             },
-//             {
-//               $sum: [
-//                 { "$multiply": [{ "$hour": "$startTime" }, 3600] },
-//                 { "$multiply": [{ "$minute": "$startTime" }, 60] },
-//                 { "$multiply": [{ "$second": "$startTime" }, 1] },
-//               ]
-//             }
-//           ]
-//         }
-//       }
-//     },
-//     },
-//   {
-//     $project: {
-//       _id: 0,
-//       tipo: "$_id",
-//       duracaoMedia: 1,
-//     }
-//   },
-// ]);
