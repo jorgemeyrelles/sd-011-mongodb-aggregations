697a25ef32d8bd98b5dc14b27da2dcb9514dedda
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-09-22 14:23:53 -0300

    desafio 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,38 @@
+db.air_alliances.aggregate([
+  { $unwind: "$airlines" },
+  {
+    $lookup: {
+      from: "air_routes",
+      let: { rotas: "$airlines" },
+      pipeline: [
+        {
+          $match: {
+            $expr: {
+              $eq: ["$$rotas", "$airline.name"] } } },
+      ],
+      as: "totalRotas",
+    },
+  },
+
+  {
+    $unwind: "$rotas",
+  },
+  {
+    $match: {
+      "$totalRotas.airplane": { $in: ["747", "380"] },
+    },
+  },
+  {
+    $group: {
+      _id: "$name",
+      totalRotas: { $sum: 1 },
+    },
+  },
+  {
+    $sort: { totalRotas: -1 },
+  },
+  {
+    $limit: 1,
+  },
+]);
+// parte de agrupamento realizado com ajuda da Gabriela Azevedo;
