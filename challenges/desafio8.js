01cfd08e4458d5aa07bf77e0ecfb755b4fcf6799
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-09-22 17:23:49 -0300

    desafio 7 - completo

---
+++ challenges/desafio8.js
@@ -0,0 +1,30 @@
+db.air_alliances.aggregate([
+  {
+    $unwind: "$airlines",
+  },
+  {
+    $lookup:
+    {
+      from: "air_routes",
+      localField: "airlines",
+      foreignField: "airline.name",
+      as: "foreign_field",
+    },
+  },
+  {
+    $unwind: "$foreign_field",
+  },
+  {
+    $match:
+    {
+      "foreign_field.airplane": { $in: ["747", "380"] },
+    },
+  },
+  {
+    $group:
+    {
+      _id: "$name",
+      totalRotas: { $sum: 1 },
+    },
+  },
+]);
