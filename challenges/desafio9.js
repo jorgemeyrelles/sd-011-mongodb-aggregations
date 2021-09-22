253665ec76498c57ef4ec8ff4c30a1e1ebd50956
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-09-22 19:04:21 -0300

    feito requisito 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,17 @@
+db.trips.aggregate([
+  { $match: { birthYear: { $ne: "" } } },
+  { $group: { 
+    _id: null,
+    maiorAno: { $max: { $toInt: "$birthYear"}},
+    menorAno: { $min: { $toInt: "$birthYear"}} 
+    } 
+  },
+  {
+    $project: {
+     maiorAnoNascimento: "$maiorAno",
+     menorAnoNascimento: "$menorAno",
+     _id: 0,
+    }
+  }
+  ]);
+  
