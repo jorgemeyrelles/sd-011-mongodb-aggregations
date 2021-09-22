2272b9a7a91c595b32345e3451c8957110351867
Daniel Leon <daniel.radialista@gmail.com>
2021-09-22 16:14:05 -0300

    desafio 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,37 @@
+const favoritos = [
+  "Sandra Bullock",
+  "Tom Hanks",
+  "Julia Roberts",
+  "Kevin Spacey",
+  "George Clooney",
+];
+
+db.movies.aggregate([
+  {
+    $match: {
+      countries: { $in: ["USA"] },
+      "tomatoes.viewer.rating": { $gte: 3 },
+      cast: { $exists: true },
+    },
+  },
+  {
+    $addFields: {
+      num_favs: {
+        $size: {
+          $setIntersection: ["$cast", favoritos], // documentação: https://docs.mongodb.com/manual/reference/operator/aggregation/setIntersection/
+        },
+      },
+    },
+  },
+  {
+    $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 },
+  },
+  { $skip: 24 },
+  { $limit: 1 },
+  {
+    $project: {
+      _id: 0,
+      title: 1,
+    },
+  },
+]);
