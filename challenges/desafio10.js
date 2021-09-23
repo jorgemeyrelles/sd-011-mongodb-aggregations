1c96f750f852745c57a72a2f1a611bb89c11798a
Naitwa Alexandre <naitwa-alexandre@gmail.com>
2021-09-23 06:55:31 -0300

    10

---
+++ challenges/desafio10.js
@@ -0,0 +1,30 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      duracao: {
+        $divide: [
+          { $subtract: ["$stopTime", "$startTime"] },
+          60 * 60 * 1000,
+        ],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: { $avg: "$duracao" },
+    },
+  },
+  {
+    $sort: {
+      duracaoMedia: 1,
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: { $round: ["$duracaoMedia", 2] },
+    },
+  },
+]);
