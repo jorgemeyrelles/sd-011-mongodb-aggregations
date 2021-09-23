d4b4ff4f970a6cbe7deea91ec5360d82a75bdeca
MatheusAraujoDev <matheusaraujo.dev@gmail.com>
2021-09-23 15:03:30 -0300

    [ADD] req 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,26 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: {
+        dia_semana: { $dayOfWeek: "$startTime" },
+        nomeEstacao: "$startStationName",
+      },
+      total: { $sum: 1 },
+    },
+  },
+  {
+    $sort: {
+      total: -1,
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      nomeEstacao: "$_id.nomeEstacao",
+      total: "$total",
+    },
+  },
+  {
+    $limit: 1,
+  },
+]);
