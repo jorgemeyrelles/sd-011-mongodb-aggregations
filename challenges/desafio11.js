58725be97f2c2c090ae4e15db18c14fa6d089e85
rodrigoruan <rodrigopython16@gmail.com>
2021-09-23 10:44:22 -0300

    Changing fields order in exercise 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -5,13 +5,13 @@ db.trips.aggregate([
       total: { $sum: 1 },
     },
   },
-  { $sort: { total: -1 } },
   {
     $project: {
-      _id: 0,
       diaDaSemana: "$_id",
-      total: 1,
+      total: "$total",
+      _id: 0,
     },
   },
+  { $sort: { total: -1 } },
   { $limit: 1 },
 ]);
