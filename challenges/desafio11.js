2da7d54ef67c4ac10be3f303ce1d7d6d6a2df669
rodrigoruan <rodrigopython16@gmail.com>
2021-09-23 10:15:56 -0300

    Added exercise 12

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,20 +1,17 @@
 db.trips.aggregate([
   {
     $group: {
-      _id: {
-        diaDaSemana: { $dayOfWeek: "$startTime" },
-        nomeEstacao: "$startStationName",
-      },
+      _id: { $dayOfWeek: "$startTime" },
       total: { $sum: 1 },
     },
   },
+  { $sort: { total: -1 } },
   {
     $project: {
       _id: 0,
-      diaDaSemana: "$_id.diaDaSemana",
+      diaDaSemana: "$_id",
       total: 1,
     },
   },
-  { $sort: { total: -1 } },
   { $limit: 1 },
 ]);
