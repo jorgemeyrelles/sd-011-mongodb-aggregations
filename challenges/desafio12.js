6dc22b1d55f9f0583368be547d0d1b3d2e28618a
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-09-23 07:11:35 -0300

    req12 realizado

---
+++ challenges/desafio12.js
@@ -0,0 +1,12 @@
+/* Desafio 12
+Usando a pipeline anterior que retorna o dia com mais viagens, 
+determine qual estação tem o maior número de viagens nesse dia da semana.
+Exiba apenas o nome da estação e o total de viagens. */
+db.trips.aggregate([
+  { $addFields: { diaDaSemana: { $dayOfWeek: "$startTime" } } },
+  { $match: { diaDaSemana: 5 } },
+  { $group: { _id: "$startStationName", total: { $sum: 1 } } },
+  { $project: { _id: 0, nomeEstacao: "$_id", total: "$total" } },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+]);
