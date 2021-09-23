99db25f03e732a83d9a3fe10eb4f5c813526e5b0
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-09-23 10:16:05 -0300

    desafio 11 - completo

---
+++ challenges/desafio11.js
@@ -0,0 +1,26 @@
+db.trips.aggregate([
+  {
+    $group:
+    {
+      _id:
+      {
+        $dayOfWeek: "$startTime",
+      },
+      totalDays: { $sum: 1 },
+    },
+  },
+  {
+    $project:
+    {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: "$totalDays",
+    },
+  },
+  {
+    $sort: { total: -1 },
+  },
+  {
+    $limit: 1,
+  },
+]);
