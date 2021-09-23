4735903a4d02aad1bdb5c097e97d7a562301b2ef
Rubia <rubiaemilly29@gmail.com>
2021-09-23 13:32:10 -0300

    ds2, 3 ,4 e 5

---
+++ challenges/desafio2.js
@@ -0,0 +1,7 @@
+db.movies.aggregate([{
+  $match: { $and: [{ "imdb.rating": { $gte: 7 } },
+    { $nor: [{ genres: "Crime" }, { genres: "Horror" }] },
+    { rated: { $in: ["PG", "G"] } },
+    { languages: { $all: ["English", "Spanish"] } }] } },
+{ $project: { _id: 0, titulo: "$title", avaliador: "$rated", notaIMDB: "$imdb.rating", votosIMDB: "$imdb.votes", ano: "$year" } },
+]);
---
+++ challenges/desafio3.js
@@ -0,0 +1,8 @@
+db.movies.aggregate([{
+  $match: { $and: [{ "imdb.rating": { $gte: 7 } },
+    { $nor: [{ genres: "Crime" }, { genres: "Horror" }] },
+    { rated: { $in: ["PG", "G"] } },
+    { languages: { $all: ["English", "Spanish"] } }] } },
+{ $project: { _id: 0, titulo: "$title", avaliador: "$rated", notaIMDB: "$imdb.rating", votosIMDB: "$imdb.votes", ano: "$year" } },
+{ $sort: { ano: -1, notaIMDB: -1, título: 1 } },
+]);
