06d8e4c4d6e45d80324eb46122b4ede6391ea7dc
Luan Xavier <luantenorio3@gmail.com>
2021-09-23 11:31:57 -0300

    Requisito 6 finalizado

---
+++ challenges/desafio6.js
@@ -0,0 +1,27 @@
+// regex source: https://www.regextutorial.org/regex-for-numbers-and-ranges.php
+
+db.movies.aggregate([
+  {
+    $match: {
+      awards: { $regex: /Won \b([0-9]|[1-9][0-9])\b Oscar[s]*/i },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      maior_rating: { $max: "$imdb.rating" },
+      menor_rating: { $min: "$imdb.rating" },
+      media_rating: { $avg: "$imdb.rating" },
+      desvio_padrao: { $stdDevSamp: "$imdb.rating" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      maior_rating: { $round: ["$maior_rating", 1] },
+      menor_rating: { $round: ["$menor_rating", 1] },
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
+    },
+  },
+]);
