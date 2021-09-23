878a7aacb1581994324fa240bc025744bf6229a6
DADA <davimarque2000@gmail.com>
2021-09-23 14:50:31 -0300

    linter de desafios 1, 2 e 3

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,15 +1,15 @@
 db.movies.aggregate(
-[
+  [
     {
-     $match: 
+     $match:
        {
-         $and: 
+         $and:
             [ 
-             { "imdb.rating": { $gte: 7 }  },
-             { $nor: [{ genres: "Crime" }, { genres: "Horror" }] },
-             { rated: { $in: ["PG", "G"] } },
-             { languages: { $all: ["English", "Spanish"] } }
-            ] 
-       } 
+              { "imdb.rating": { $gte: 7 }  },
+              { $nor: [{ genres: "Crime" }, { genres: "Horror" }] },
+              { rated: { $in: ["PG", "G"] } },
+              { languages: { $all: ["English", "Spanish"] } },
+            ],
+       },
     },
-]);
+  ]);
--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,33 +1,33 @@
 db.movies.aggregate(
-    [
-        {
-         $match: 
+  [
+    {
+      $match:
            {
-             $and: 
-                [ 
-                 { "imdb.rating": { $gte: 7 }  },
-                 { $nor: [{ genres: "Crime" }, { genres: "Horror" }] },
-                 { rated: { $in: ["PG", "G"] } },
-                 { languages: { $all: ["English", "Spanish"] } }
-                ] 
-           } 
+             $and:
+                [
+                  { "imdb.rating": { $gte: 7 } },
+                  { $nor: [{ genres: "Crime" }, { genres: "Horror" }] },
+                  { rated: { $in: ["PG", "G"] } },
+                  { languages: { $all: ["English", "Spanish"] } },
+                ],
+           },
+    },
+    {
+      $addFields: {
+        titulo: "$title",
+        avaliado: "$rated",
+        notaIMDB: "$imdb.rating",           
+        votosIMDB: "$imdb.votes",
+        ano: "$year",
         },
-        {
-            $addFields: {
-              titulo: "$title",
-              avaliado: "$rated",
-              notaIMDB: "$imdb.rating",
-              votosIMDB: "$imdb.votes",
-              ano: "$year",
-            },
-        },
-        {
-            $project : {
-              titulo : 1,
-              avaliado : 1,
-              notaIMDB: 1,
-              votosIMDB: 1,
-              ano: 1,
-            }
-        },
-    ]);
+      },
+      {
+      $project: {
+        titulo : 1,
+        avaliado : 1,
+        notaIMDB: 1,
+        votosIMDB: 1,
+        ano: 1,
+      },
+    },
+  ]);
