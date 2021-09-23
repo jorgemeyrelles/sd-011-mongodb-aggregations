95051919562f2a8980cdddde2b7814c83e7e108d
Ivaldo Souza <ivaldo.souza@datapar.com.br>
2021-09-23 08:12:41 -0300

    Desafios 1 - 4

---
+++ challenges/desafio3.js
@@ -0,0 +1,34 @@
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [{ "imdb.rating": { $gte: 7 } },
+        { $nor: [{ genres: "Crime" }, { genres: "Horror" }] },
+        { rated: { $in: ["PG", "G"] } },
+        { languages: { $all: ["English", "Spanish"] } }],
+    },
+  },
+  {
+    $addFields: {
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
+  {
+    $project: {
+      titulo: 1,
+      avaliado: 1,
+      notaIMDB: 1,
+      votosIMDB: 1,
+      ano: 1,
+      _id: 0,
+    },
+  },
+  {
+    $sort: {
+      ano: -1, notaIMDB: -1, título: 1,
+    },
+  },
+]);
