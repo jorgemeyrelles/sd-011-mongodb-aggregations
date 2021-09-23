e8c5d66e8ba6d2653446503586de20e97b31cdfb
Amandha W. Barok <amandha.barok@gmail.com>
2021-09-23 10:58:01 -0700

    fixing errors

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -10,12 +10,11 @@ db.trips.aggregate([
   },
   { $sort: { total: -1 } },
   { $limit: 1 },
-  { 
+  {
     $project: {
       nomeEstacao: "$_id.nomeEstacao",
       total: "$total",
       _id: 0,
-    }
+    },
   },
 ]);
-
